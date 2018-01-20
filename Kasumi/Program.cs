826b0dcc80c60fe3bbf38ddaab769ec5b4f00d98
Kotori7 <me@kotori.zone>
2018-01-20 13:12:10 +0000

    Add project files.

---
+++ Kasumi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kasumi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
